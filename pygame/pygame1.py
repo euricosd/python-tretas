4a638433d0e8eb112a45470ebf5d801af4344c4f
Eurico Serradas Duarte <eurico@serradasduarte.ga>
2020-05-29 11:10:38 +0100

    Novos tamanhos. Em beta Tentar colocar   retângulo com tamanhos diferentes.

--- pygame/pygame1.py
+++ pygame/pygame1.py
@@ -5,7 +5,7 @@ pygame.init()
 
 window = pygame.display.set_mode((500, 400))
 
-pygame.draw.rect(window, (255,0,0), (0,0,50,30))
+pygame.draw.rect(window, (255,0,0), (0,0,400,100))
 
 pygame.display.update()
 
